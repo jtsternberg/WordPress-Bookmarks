ed2dedb09cc3af3d3b96e24421ff27fd00063264
Justin Sternberg <justin@dsgnwrks.pro>
2015-06-02 13:45:32 -0400

    Lots of modifications (and a lame commit message)

--- includes/press-this.php
+++ includes/press-this.php
@@ -14,12 +14,70 @@ class WPB_Press_This {
 	protected $link = '';
 
 	/**
+	 * The post terms array
+	 * @var array
+	 */
+	protected $terms = array();
+
+	/**
+	 * Existing post id if found
+	 * @var int
+	 */
+	protected $existing_bookmark_id = 0;
+
+	/**
 	 * Initiate our hooks
 	 * @since 0.1.0
 	 */
 	public function hooks() {
 		add_filter( 'press_this_data', array( $this, 'get_link' ) );
-		add_action( 'save_post', array( $this, 'save_link_to_excerpt' ) );
+		add_action( 'wp_insert_post_data', array( $this, 'save_link_to_excerpt' ), 10, 2 );
+		add_action( 'save_post', array( $this, 'set_post_format' ), 99 );
+		add_action( 'set_object_terms', array( $this, 'save_all_category_parents' ), 10, 4 );
+
+		add_action( 'admin_print_styles-press-this.php', array( $this, 'custom_styles' ) );
+		add_action( 'admin_footer-press-this.php', array( $this, 'custom_js' ) );
+	}
+
+	public function custom_styles() {
+		?>
+		<style type="text/css">
+			li.popular-category > div {
+				background: rgba(0, 187, 255, 0.18);
+			}
+			.categories-select li {
+				background: rgba(0, 187, 255, 0.1);
+				background: rgba(0, 160, 210, 0.1);
+				padding-left: 10px;
+			}
+			.category {
+				border-bottom: 1px solid #ddd;
+			}
+			.categories-select ul .category {
+			  padding-left: 24px !important;
+			}
+		</style>
+		<?php
+	}
+
+	public function custom_js() {
+
+		if ( $this->existing_bookmark_id ) {
+			$edit_link = get_edit_post_link( $this->existing_bookmark_id );
+			$message = sprintf( __( 'Found a bookmark with this URL. <a href="%s">%s</a>?', 'wordpress-bookmarks' ), esc_url( $edit_link ), __( 'Edit Instead', 'wordpress-bookmarks' ) );
+		}
+
+		?>
+		<script type="text/javascript">
+		jQuery(document).ready(function($){
+			$( '.post-format-link' ).trigger( 'click' );
+			$( '.dashicons.dashicons-category' ).parents( '.post-options .post-option' ).trigger( 'click' );
+			<?php if ( $this->existing_bookmark_id ) : ?>
+			$( document.getElementById( 'title-container' ) ).before( '<p id="message" class="update"><?php echo $message ?></p>' );
+			<?php endif; ?>
+		});
+		</script>
+		<?php
 	}
 
 	/**
@@ -52,25 +110,62 @@ class WPB_Press_This {
 	 *
 	 * @param  int  $post_id The post ID
 	 */
-	public function save_link_to_excerpt( $post_id ) {
-		static $prevent_recursion = false;
-
+	public function save_link_to_excerpt( $data, $postarr ) {
 		// if we match our conditions
 		if (
-			! $prevent_recursion
-			&& $this->link
+			$this->link
 			&& ! empty( $_POST['_links']['canonical'] )
 			&& $this->link == $_POST['_links']['canonical']
 		) {
 
-			$prevent_recursion = true;
+			$data['post_excerpt'] = $this->link;
 
-			// then update the bookmark post
-			$updated = wp_update_post( array(
-				'ID'           => $post_id,
-				'post_excerpt' => $this->link,
-			), true );
+			$posts = get_posts( array(
+				'posts_per_page' => 1,
+				'meta_key'       => 'bookmark_url',
+				'meta_value'     => $this->link,
+				'fields'         => 'ids',
+			) );
 
+			if ( ! empty( $posts ) ) {
+				$this->existing_bookmark_id = $posts[0];
+			}
+
+		}
+
+		return $data;
+	}
+
+	public function set_post_format( $post_id ) {
+		if ( $this->link ) {
+			set_post_format( $post_id, 'link' );
+			update_post_meta( $post_id, 'bookmark_url', $this->link );
+		}
+	}
+
+	public function save_all_category_parents( $post_id, $terms, $tt_ids, $taxonomy ) {
+		static $prevent_recursion = false;
+
+		if ( $prevent_recursion || 'category' != $taxonomy || empty( $terms ) || ! get_post_meta( $post_id, 'bookmark_url', 1 ) ) {
+			return;
+		}
+
+		$prevent_recursion = true;
+
+		$this->terms = array();
+		foreach ( $terms as $term_id ) {
+			$this->get_parents( $term_id );
+		}
+
+		wp_set_post_categories( $post_id, $this->terms );
+	}
+
+	public function get_parents( $term_id ) {
+		$this->terms[] = $term_id;
+		$term = get_term( $term_id, 'category' );
+
+		if ( $term->parent ) {
+			$this->get_parents( $term->parent );
 		}
 	}
 
